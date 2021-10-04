bdddcacc5a073e9ae1ee519834f2247da56ba32b
Yves Delacretaz <yves.delacretaz@alpvision.com>
2021-10-04 15:05:40 +0200

    PharmaledgerMessageHandler for SSAPP

--- WkCamera/WkCamera/CameraWebViewController.swift
+++ WkCamera/WkCamera/CameraWebViewController.swift
@@ -9,11 +9,12 @@ import Foundation
 import AVFoundation
 import UIKit
 import WebKit
+import PharmaLedger_Camera
 
 public class CameraWebViewController: UIViewController, WKUIDelegate {
     // MARK: Privates
     private var webview: WKWebView?
-    private var messageHandler = JsMessageHandler(staticPath: Bundle.main.path(forResource: "www", ofType: nil))
+    private var messageHandler = PharmaledgerMessageHandler(staticPath: Bundle.main.path(forResource: "www", ofType: nil))
     
     func load() {
         self.webview = messageHandler.getWebview(frame: self.view.frame)
@@ -22,7 +23,7 @@ public class CameraWebViewController: UIViewController, WKUIDelegate {
 //            let fileUrl = URL(fileURLWithPath: Bundle.main.path(forResource: "www/bootstrap.html", ofType: nil)!)
 //            webview.loadFileURL(fileUrl, allowingReadAccessTo: fileUrl.deletingLastPathComponent())
             
-            let url = URL(string: "http://localhost:\(self.messageHandler.webserver.port)/bootstrap.html")!
+            let url = URL(string: "http://localhost:\(self.messageHandler.webserverPort)/bootstrap.html")!
             webview.load(URLRequest(url: url))
             
             self.view.addSubview(webview)
